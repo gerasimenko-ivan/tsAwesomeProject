a6a45b201e4221d42f415896e495e8b2466b054f
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-04-27 20:59:10 +0300

    add logs & comments

--- src/func/func-as-param.ts
+++ src/func/func-as-param.ts
@@ -4,27 +4,31 @@ function step1(): void {
 function step2(): void {
     console.log('step 2');
 }
+// step 3 - versions of function :: void > void
 function step3_v1(): void {
-    console.log('step 3 - v1');
+    console.log('step 3: v1 (void > void)');
 }
 function step3_v2(): void {
-    console.log('step 3 - v2');
+    console.log('step 3: v2 (void > void) // just logs diff texts');
 }
+// step 4 - versions of function :: string, string > number
 function step4_v1(a: string, b: string): number {
     const s = a + b;
-    console.log(`step 4 - v1 - a+b=${s}`);
+    console.log(`step 4: v1 (string, string > number)`);
+    console.log(`step 4: a+b='${s}'`);
     return s.length;
 }
 function step4_v2(a: string, b: string): number {
     const s = a + b + a;
-    console.log(`step 4 - v1 - a+b+a=${s}`);
+    console.log(`step 4: v2 (string, string > number)`);
+    console.log(`step 4: a+b+a='${s}' // for same input: logs diff texts & gives diff output`);
     return s.length;
 }
 function step5(n: number): void {
     console.log(`step 5 - number: ${n}`);
 }
 
-function test(step3: { (): void}, step4: {(x, y): number}): void {
+function testStepByStep(step3: { (): void}, step4: {(x, y): number}): void {
     step1();
     step2();
     step3();
@@ -32,7 +36,8 @@ function test(step3: { (): void}, step4: {(x, y): number}): void {
     step5(number);
 }
 
-console.log('>>> test - v1');
-test(step3_v1, step4_v1);
-console.log('\n>>> test - v2');
-test(step3_v2, step4_v2);
+console.log('>>> testStepByStep - with v1-s');
+testStepByStep(step3_v1, step4_v1);
+console.log('\n');
+console.log('>>> testStepByStep - with v2-s');
+testStepByStep(step3_v2, step4_v2);
