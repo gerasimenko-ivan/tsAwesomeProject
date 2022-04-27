6ef738b2e7d6bb27c6b1419f71a1317e52aca7d7
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-04-27 21:11:29 +0300

    add todo - learn more about Mocha

---
+++ src/mocha/_doc.js
@@ -0,0 +1,14 @@
+/**
+ * Mocha is a JavaScript test framework for Node.js
+ * with features like
+ *  - browser support,
+ *  - asynchronous testing,
+ *  - test coverage report,
+ *  - and use of any assertion library.
+ *
+ *
+ * TODO: How to:
+ *  - afterEach() ??? (in func calls, ...) TODO: how to say 'вложенность' in english?
+ *  - describe ???
+ *  - it ???
+ */
